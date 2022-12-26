e5c428664052faa12278124477425f6ca2ab2d57
txnt3ch <txntech@gmail.com>
2022-12-26 22:45:52 +0700

    Initiate the project

---
+++ src/main/java/com/txn/helloservice/HelloserviceApplication.java
@@ -0,0 +1,13 @@
+package com.txn.helloservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloserviceApplication.class, args);
+	}
+
+}
